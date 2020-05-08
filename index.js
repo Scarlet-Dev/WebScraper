2df639ebea38e04dc603b0cd8f2d632aeeddef60
Scarlet <developerscarlet@gmail.com>
2020-05-08 00:34:33 -0400

    Now scrapes listed links in article.

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 const fs = require('fs');
+const Article = require("./gramblinite-links.json");
+const axios = require("axios").default;
+const cheerio = require('cheerio');
 
-import Article from "./gramblinite-links.json";
-import Axios from "axios";
-
-const articleLinks = [];
-articleLinks = articleLinks.pop(Article["published-works"].forEach(i => { return i.link}));
-
-function saveHtmlToFile(){
-
-}
 
 function main() {
+    var articleLinks = new Array();
+    articleLinks = Article["published-works"].map(i => {return i.link});
     articleLinks.map((link, i) => {
+        
         let filename = "link"+"_"+i+".html";
-        let htmlData = await Axios.get(link);
-        fs.writeFile(filename, htmlData, function(err){
-            if(err) console.log(err);
-            console.log("Html found!")
+        let path = "htmlSource";
+
+        var pathToFile = path+"/"+filename;
+
+        axios.get(link)
+        .then(response => response.data)
+        .then(data => { 
+            fs.writeFile(pathToFile, data, function(err){
+                if(err) console.log(err);
+                console.log("Html found!")
+            });
         })
+        .catch(err => console.log(err));
+
     })
 }
+
+main()
